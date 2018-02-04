737b3e5d27dd02a27cb23a84b74d5839ed5544ad
angryBulls <sundonghaistrong@163.com>
2018-02-04 18:29:00 +0800

    add kongge

---
+++ lilong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lilong
+//
+//  Created by 孙中山 on 2018/2/4.
+//  Copyright © 2018年 孙中山. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lilong/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lilong
+//
+//  Created by 孙中山 on 2018/2/4.
+//  Copyright © 2018年 孙中山. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
